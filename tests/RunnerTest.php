d07a59fe716fa6220c7a39ab1ad1ef2e3e0781d6
insolita <webmaster100500@ya.ru>
2018-04-11 00:39:32 +0800

    ensure test report files deleted

--- tests/RunnerTest.php
+++ tests/RunnerTest.php
@@ -20,9 +20,12 @@ class RunnerTest extends TestCase
     public function testItShouldBeStoreJsonReport()
     {
         Carbon::setTestNow('2018-01-02 03:04:05');
+        $reportFile = __DIR__ . '/reports/package_usage_report_2018-01-02_03_04.json';
+        if(file_exists($reportFile)){
+            unlink($reportFile);
+        }
         $exitCode = (new Runner(__DIR__ . '/scanner_test_config_reported.php', false))->run();
         $this->assertEquals(Runner::SUCCESS_CODE, $exitCode);
-        $reportFile = __DIR__ . '/reports/package_usage_report_2018-01-02_03_04.json';
         $this->assertFileExists($reportFile);
         $fileData = json_decode(file_get_contents($reportFile), true);
         print_r($fileData);
@@ -31,9 +34,12 @@ class RunnerTest extends TestCase
     public function testItShouldBeStoreCustomFormattedReport()
     {
         Carbon::setTestNow('2018-01-02 03:04:05');
+        $reportFile = __DIR__ . '/reports/package_usage_report_2018-01-02_03_04.txt';
+        if(file_exists($reportFile)){
+            unlink($reportFile);
+        }
         $exitCode = (new Runner(__DIR__ . '/scanner_test_config_reported_custom.php', false))->run();
         $this->assertEquals(Runner::SUCCESS_CODE, $exitCode);
-        $reportFile = __DIR__ . '/reports/package_usage_report_2018-01-02_03_04.txt';
         $this->assertFileExists($reportFile);
         $fileData = file_get_contents($reportFile);
         print_r($fileData);
