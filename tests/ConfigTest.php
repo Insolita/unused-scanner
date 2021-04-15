f8bf0d82e0f89b825c3fc72383bc26e54857b134
insolita <webmaster100500@ya.ru>
2021-04-15 20:13:58 +0800

    fix test

--- tests/ConfigTest.php
+++ tests/ConfigTest.php
@@ -12,8 +12,8 @@ class ConfigTest extends TestCase
         $params = require __DIR__ . '/scanner_test_config_dev.php';
         $config = Config::create($params);
         $dirs = $config->getScanDirectories();
-        $this->assertContains(__DIR__.'/stubs/', $dirs);
-        $this->assertNotContains(__DIR__.'/not_existed/', $dirs);
-        $this->assertNotContains(__DIR__.'/missing/', $dirs);
+        $this->assertContains(__DIR__ . '/stubs', $dirs);
+        $this->assertNotContains(__DIR__ . '/not_existed', $dirs);
+        $this->assertNotContains(__DIR__ . '/missing', $dirs);
     }
 }
